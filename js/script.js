c216c3be7365ebed86602e484827bd7c52836520
Maxkray <36783635+Maxkray@users.noreply.github.com>
2018-04-01 19:53:08 +0300

    Исправление к Сдаче

--- js/script.js
+++ js/script.js
@@ -7,20 +7,44 @@
   var name = popup.querySelector("[name=name]");
   var mail = popup.querySelector("[name=mail]");
 
+  var isStorageSupport = true;
+  var storage = "";
+
+  try {
+    storage = localStorage.getItem("mail");
+  } catch (err) {
+    isStorageSupport = false;
+  }
+
   link.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.add("modal-show");
     name.focus();
+
+    if (storage) {
+      mail.value = storage;
+      password.focus();
+    } else {
+      mail.focus();
+    }
   });
 
   close.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.remove("modal-show");
+    popup.classList.remove("modal-error");
   });
 
   form.addEventListener("submit", function (evt) {
     if (!name.value || !mail.value) {
       evt.preventDefault();
-      console.log("Нужно ввести логин и пароль");
+      popup.classList.remove("modal-error");
+      popup.offsetWidth = popup.offsetWidth;
+      popup.classList.add("modal-error");
+    } else {
+      if (isStorageSupport) {
+        localStorage.setItem("mail", mail.value);
+      }
     }
   });
+
